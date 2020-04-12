39cb768ee119077a23a94e4611a3d22eefba7902
nigel grey <ohm@nigels-MacBook-Air.local>
2020-04-12 15:48:05 -0700

    Added exceptions

--- testDB.py
+++ testDB.py
@@ -7,33 +7,61 @@ conn = lite.connect(DATABASE_NAME)
 c = conn.cursor()
 
 def createTable():
-    c.execute('''CREATE TABLE IF NOT EXISTS USERS (login TEXT PRIMARY KEY, password TEXT)''')
+    try:
+        c.execute('''CREATE TABLE IF NOT EXISTS USERS (login TEXT PRIMARY KEY, password TEXT)''')
+    except lite.error as msg:
+        print("Create table failed. Error: " + str(msg))
+        sys.exit()
 
 def createUser(username, password):
-    if not duplicateUser(user):
-        c.execute('INSERT INTO USERS (login, password) values (?, ?)', (username, password))
-        print("User created successfully")
-    else:
-        print("Duplicate user")
+    try:
+        if not duplicateUser(user):
+            c.execute('INSERT INTO USERS (login, password) values (?, ?)', (username, password))
+            print("User created successfully")
+        else:
+            print("Duplicate user")
+    except lite.error as msg:
+        print("Create user failed. Error: " + str(msg))
+        sys.exit()
 
 def getUsers():
-    result = c.execute("SELECT login, password from USERS")
+    try:
+        result = c.execute("SELECT login, password from USERS")
+    except lite.error as msg:
+        print("Get users failed. Error: " + str(msg))
+        sys.exit()
 
-    for row in result:
-        print("username = {}\npassword = {}".format(row[0], row[1]))
+    finally:
+        for row in result:
+            print("username = {}\npassword = {}".format(row[0], row[1]))
 
 def getUser(username):
-    result = c.execute("SELECT * FROM USERS WHERE login = ?", (username, ))
+    try:
+        result = c.execute("SELECT * FROM USERS WHERE login = ?", (username, ))
+    except lite.error as msg:
+        print("Get user failed. Error: " + str(msg))
+        sys.exit()
+
     return result.fetchone()
 
 def getPassword(username):
-    user = getUser(username)
-    if user != None:
-        return user[1]
+    try:
+        user = getUser(username)
+    except lite.error as msg:
+        print("Get password failed. Error: " + str(msg))
+
+    finally:
+        if user != None:
+            return user[1]
+
     return None
 
 def duplicateUser(username):
-    result = getUser(username)
+    try:
+        result = getUser(username)
+    except lite.error as msg:
+        print("Duplicate check failed. Error: " + str(msg))
+
     return result != None
 
 createTable()
