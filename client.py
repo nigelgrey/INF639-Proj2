227c1f31da682017ba7febf81f7a10d90a9029bd
Jack Garrard <jg2562@nau.edu>
2020-04-14 23:32:53 -0700

    Fixed up client to use other socket method

--- client.py
+++ client.py
@@ -8,14 +8,25 @@ import hashlib
 
 HOST = ''
 PORT = 8888
+FILE = "/tmp/inf-test"
+key_size = 128
+bind_arg = FILE
+# bind_arg = ((host,port))
 
-def connectSocket(host, port):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((host,port))
-	return s
+def createSocket(sock_type,bind_args):
+    s = socket.socket(sock_type, socket.SOCK_STREAM)
+
+    try:
+        s.bind(bind_args)
+    except socket.error as msg:
+        print('Bind failed. Error Code : ' + str(msg))
+        sys.exit()
+
+    s.listen(10)
+    return s
 
 
-s = connectSocket(HOST,PORT)
+s = connectSocket(socket.AF_UNIX, bind_arg)
 try:
     # Asks user for info (or parses from command line)
     user = input("Enter username: ")
@@ -23,6 +34,7 @@ try:
     # Opens socket to server requesting auth, sending username/password
     # Waits for auth
     # Tells user if successful
+	
 finally:
 	s.shutdown(socket.SHUT_RDWR)
 	s.close()
