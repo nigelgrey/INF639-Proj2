67ba11894176146e0a36f0053412fac2e06523ee
Jack Garrard <jg2562@nau.edu>
2020-04-14 17:11:13 -0700

    Fixed indention errors

--- server.py
+++ server.py
@@ -31,26 +31,26 @@ def createSocket():
 
 
 def listenForClient(s):
-	conn, addr = s.accept()
-	data = conn.recv(2000)
-	client_data = pickle.loads(data)
-	hashed = hashClientData(client_data)
-	puffed_hashes = lookupPufHashes(hashed)
-	valid = lookupDatabase(puffed_hashes)
+    conn, addr = s.accept()
+    data = conn.recv(2000)
+    client_data = pickle.loads(data)
+    hashed = hashClientData(client_data)
+    puffed_hashes = lookupPufHashes(hashed)
+    valid = lookupDatabase(puffed_hashes)
 
 def hashClientData(client_data):
-	hash_user = hash(client_data.username)
-	hash_pass = hash(client_data.password)
-	xored = hash_user ^ hash_pass
-	hash_xor = hash(xored)
-	hash_data = HashData(hash_xor, hash_pass)
-	return hash_data
+    hash_user = hash(client_data.username)
+    hash_pass = hash(client_data.password)
+    xored = hash_user ^ hash_pass
+    hash_xor = hash(xored)
+    hash_data = HashData(hash_xor, hash_pass)
+    return hash_data
 
 def lookupPufHashes(hash_data):
-	pass
+    pass
 
 def lookupDatabase(puffed_hashes):
-	pass
+    pass
 
 s = createSocket()
 print("Connected to server")
